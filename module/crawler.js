e4052e9b8a8f50cd5791c1a01c198ccca0ca764d
zhaopeng <peng.zpz@gmail.com>
2015-10-23 00:57:11 +0800

    test crawler

--- module/crawler.js
+++ module/crawler.js
@@ -15,7 +15,17 @@ Crawler.prototype.getWeiBoFans = function () {
 
   http.get(this.url, function httpGet(res) {
     var html = '';
-    console.log(res);
+    for (var key in res){
+      if(typeof res[key] != 'function' ) {
+        console.log('--\n',key,'\n',res[key],'\n--');
+      }else{
+        console.log('--\n',key,'\n','\033[33mfunction\033[0m','\n--');
+
+      }
+
+    }
+    //console.log('-------------');
+    //console.log('--\n',res.headers,'\n--');
     res.on('data', function resOnData(data) {
       html += data;
     });
@@ -32,6 +42,7 @@ Crawler.prototype.getWeiBoFans = function () {
     console.log('get data error .');
 
   });
+  return fansNumber;
 };
 
 module.exports = Crawler;
--- module/start.js
+++ module/start.js
@@ -1,6 +1,6 @@
 
 var Crawler = require('./crawler');
-var url = 'http://passport.weibo.com/visitor/visitor?entry=miniblog&a=enter&url=http%3A%2F%2Fweibo.com%2Fu%2F2196095270&domain=.weibo.com&ua=php-sso_sdk_client-0.6.14&_rand=1445448998.5807';
+var url = 'http://weibo.com/2196095270/profile/';
 var weiboCrawler = new Crawler(url);
 var title = '';
 
