89016e5577f2abd1281ef09e098a30a1498a5618
zhaopeng <zhaopeng@romens.cn>
2016-07-12 20:15:27 +0800

    add file log support

--- bin/socketio.client.js
+++ bin/socketio.client.js
@@ -1,21 +1,24 @@
 var io = require("socket.io-client");
+var fs = require("fs");
 
 var serverUrl = "http://192.168.100.220:8886";
+var connNumber = 0;                     // 链接尝试次数
+var lastConnectTime = new Date();
 
 var option = {
-    reconnection: true,             // 是否重连
+    reconnection: true,                 // 是否重连
     reconnectionAttempts: Infinity,     // 停止重连前的尝试时间
-    reconnectionDelay: 2000,        // 重连前的延迟
-    reconnectionDelayMax: 10000,     // 重连的最大间隔时间
-    randomizationFactor: 0.5,      // 随机范围
-    timeout: 100,                  // 超时时间,超时之后会触发 connect_timeout or connect_error
-    autoConnect: true               // 自动重连
+    reconnectionDelay: 2 * 1000,        // 重连前的延迟
+    reconnectionDelayMax: 60 * 1000,    // 重连的最大间隔时间
+    randomizationFactor: 0.5,           // 随机范围
+    timeout: 3 * 1000,                  // 超时时间,超时之后会触发 connect_timeout or connect_error
+    autoConnect: true                   // 自动重连
 };
 
+fs.writeFileSync("./socketClient.log", "");     // 清空log文件
+printLog("程序: serverUrl: " + serverUrl);
+printLog("程序: 尝试链接服务器: " + now());
 
-
-console.log("程序: serverUrl: " + serverUrl);
-console.log("程序: 尝试链接服务器: " + now());
 var socket = io(serverUrl, option);
 
 socket.on("connect", onConnect);
@@ -32,58 +35,70 @@ socket.on("pong", pong);
 
 
 function onConnect() {
-    console.log("事件: 成功链接到服务器: " + now());
+    printLog("事件: 成功链接到服务器: " + now());
 }
 
 function onDisconnect() {
-    console.log("事件: 与服务器断开链接: " + now());
+    printLog("事件: 与服务器断开链接: " + now());
 }
 
 function connect_error(error) {
-    console.log("事件: 链接错误: " + now());
-    console.log("\t" + error);
+    printLog("事件: 链接错误: " + now());
+    printLog("\t" + error);
 }
 
 function connect_timeout() {
-    console.log("事件: 连接超时: " + now());
+    printLog("事件: 连接超时: " + now());
 }
 
 function reconnect(attemptNumber) {
-    console.log("事件: 重新连接成功: " + now() + ' 第n次尝试:' + attemptNumber);
+    printLog("事件: 重新连接成功: " + now() + ' 第n次尝试:' + attemptNumber);
 }
 
 function reconnect_attempt() {
-
-    console.log();
-    console.log();
-
-    console.log("事件: 尝试重新连接: " + now());
+    printLog();
+    printLog();
+    printLog("事件: 尝试重新连接: " + now());
+    var n = new Date();
+    printLog("\t" + "目前是第 " + connNumber++ + " 次, 距离上次的间隔:" + Math.floor((n - lastConnectTime)/1000));
+    lastConnectTime = n;
 }
 
 function reconnecting() {
-    console.log("事件: 正在重新连接: " + now());
+    printLog("事件: 正在重新连接: " + now());
 }
 
 function reconnect_error(error) {
-    console.log("事件: 重连出错: " + now());
-    console.log("\t" + error);
+    printLog("事件: 重连出错: " + now());
+    printLog("\t" + error);
 }
 
 function reconnect_failed() {
-    console.log("事件: 重连失败: " + now());
+    printLog("事件: 重连失败: " + now());
 }
 
 function ping() {
-    console.log("事件: ping: " + now());
+    printLog("事件: ping: " + now());
 }
 
 function pong(number) {
-    console.log("事件: pong: " + now() + " 毫秒数:" + number);
+    printLog("事件: pong: " + now() + " 毫秒数:" + number);
 }
 
 
 function now() {
     var no = new Date();
-    return no.toLocaleString() + "." + no.getMilliseconds();
+    // return no.toLocaleString() + "." + no.getMilliseconds();
+    return no.toLocaleString();
 }
 
+function printLog(log) {
+    log = log || "";
+    var filePath = "./socketClient.log";
+    saveLogToFile(filePath, log);
+    console.log(log);
+}
+
+function saveLogToFile(filePath, log) {
+    return fs.appendFileSync(filePath, log + '\n');
+}
